c3ce2973b043746e79f2c95a50bac46f6977a052
Benedict Hartley <spam@chaffin.ch>
2016-03-20 06:32:25 +0000

    Add notification for battery < 15%

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 local wibox = require("wibox")
+local naughty = require("naughty")
 
 local widget = wibox.widget.textbox()    
 local poll = timer({timeout = 10})    
@@ -8,7 +9,21 @@ widget:set_text(" | Battery | ")
 poll:connect_signal("timeout",    
 function()    
     local fh = assert(io.popen("acpi | cut -d, -f 2 -", "r"))    
-    widget:set_text(" |" .. fh:read("*l") .. " | ")    
+    local percentage = string.gsub(fh:read("*l"), "%s+", "")
+    local as_num = string.gsub(percentage, "%%", "")
+
+    if tonumber(as_num) < 16 then
+        naughty.notify({
+            preset = naughty.config.presets.critical,
+            title = "Battery",
+            text = "Recharge / suspend now",
+            timeout = 10
+        })
+        widget:set_markup(' | <span color="red">' .. percentage .. '</span> | ')
+    else
+        widget:set_text(" | " .. percentage .. " | ")
+    end
+
     fh:close()    
 end    
 )    
